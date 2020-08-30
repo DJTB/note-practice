f4dac3b6bec51a8e2a0f3ba23cf197ea97f2fea7
Duncan Bay <duncan.bay@gmail.com>
2020-08-31 09:15:10 +1000

    feat: close mobile keyboard on number input submit

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,20 +1,26 @@
-import React, { ChangeEventHandler, FC } from 'react';
+import React, { ChangeEventHandler, FC, useRef } from 'react';
 import { SettingsLabel } from './SettingsLabel';
 
 export const Count: FC<{
   value: number;
   onChange: ChangeEventHandler;
-}> = ({ value, onChange }) => (
-  <>
-    <SettingsLabel name="note-count">Count:</SettingsLabel>
-    <input
-      className="w-10 px-1 rounded-sm"
-      name="note-count"
-      type="number"
-      min={1}
-      max={12}
-      value={value}
-      onChange={onChange}
-    />
-  </>
-);
+}> = ({ value, onChange }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <SettingsLabel name="note-count">Count:</SettingsLabel>
+      <input
+        ref={inputRef}
+        className="w-10 px-1 rounded-sm"
+        name="note-count"
+        type="number"
+        min={1}
+        max={12}
+        value={value}
+        onChange={onChange}
+        onSubmit={() => inputRef.current?.blur()}
+      />
+    </>
+  );
+};
