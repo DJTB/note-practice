79a562d6dd459438f7b41e7b5874c2b54695c715
Duncan Bay <duncan.bay@gmail.com>
2020-08-30 18:57:51 +1000

    Updates

---
+++ precache-manifest.4d8ed850b7562618615bc7af3b4de2a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f99107c40b8f8be9447e1ede4d3e154c",
+    "url": "/note-practice/index.html"
+  },
+  {
+    "revision": "e7e0be741418364a37a3",
+    "url": "/note-practice/static/css/main.8532d014.chunk.css"
+  },
+  {
+    "revision": "24410fee8b72e432b0d0",
+    "url": "/note-practice/static/js/2.d35c9ec1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/note-practice/static/js/2.d35c9ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e0be741418364a37a3",
+    "url": "/note-practice/static/js/main.d8919415.chunk.js"
+  },
+  {
+    "revision": "79c227e92f7d64e46175",
+    "url": "/note-practice/static/js/runtime-main.b2da3c55.js"
+  }
+]);
